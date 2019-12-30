d0bb4b470ece502f33b54e16fb99a47cf9037f01
LimitToInfinity <21stLimitless@gmail.com>
2019-12-30 13:16:47 -0700

    Added node-sass npm and removed boiler plate React initialization.

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './../stylesheets/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Present Truths</h1>
+    </div>
+  );
+}
+
+export default App;
