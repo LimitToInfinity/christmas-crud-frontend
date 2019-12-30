6b9d8a55e23923cdca3d0fd248a382ec814988be
LimitToInfinity <21stLimitless@gmail.com>
2019-12-30 14:38:35 -0700

    Added cards and ratings for presents.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './../stylesheets/App.scss';
+import PresentCard from './PresentCard';
 
 const presentsURL = "http://localhost:3000/presents/"
 
@@ -12,12 +13,22 @@ class App extends Component {
       .then(parseJSON)
       .then(presents => this.setState({ presents: extractData(presents) }));
   }
+
+  createPresentCards = () => {
+    const { presents } = this.state;
+    return presents.map(present => {
+      return (
+       <PresentCard key={ present.id } present={ present } />
+      );
+    })
+  }
   
   render() {
     
     return (
       <div className="App">
         <h1>Present Truths</h1>
+        { this.createPresentCards() }
       </div>
     );
   }
---
+++ src/components/PresentCard.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function PresentCard({ present }) {
+
+    const createRatings = () => {
+        return present.ratings.map(rating => {
+            return (
+                <div className="ratings">
+                    <p>{ rating.stars} </p>
+                    <p>{ rating.description} </p>
+                </div>
+            );
+        })
+    }
+  
+    return (
+        <div className="present-card">
+            <h2>{ present.name }</h2>
+            <img alt={ present.name } src={ present.image } />
+            { createRatings() }
+        </div>
+    );
+}
+
+export default PresentCard;
