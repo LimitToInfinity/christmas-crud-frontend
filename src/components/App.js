33bd4c8728e6a84d06d3fdfc9c0f407c5a2273ea
LimitToInfinity <21stLimitless@gmail.com>
2019-12-30 14:07:15 -0700

    Fetch request.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './../stylesheets/App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Present Truths</h1>
-    </div>
-  );
+const presentsURL = "http://localhost:3000/presents/"
+
+class App extends Component {
+
+  state = { presents: [] }
+
+  componentDidMount() {
+    fetch(presentsURL)
+      .then(parseJSON)
+      .then(presents => this.setState({ presents: extractData(presents) }));
+  }
+  
+  render() {
+    
+    return (
+      <div className="App">
+        <h1>Present Truths</h1>
+      </div>
+    );
+  }
+}
+
+function extractData(fastJson) {
+  return fastJson.data.map(unNest);
+}
+
+function unNest(instance) {
+  return instance.attributes;
+}
+
+function parseJSON(response) {
+  return response.json();
 }
 
 export default App;
